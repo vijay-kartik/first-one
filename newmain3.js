198c216108be469ce573e0266c21a781d5eea493
Kartik <kartik.iit96@gmail.com>
2017-05-12 22:03:50 +0530

    Update newmain3.js

--- newmain3.js
+++ newmain3.js
@@ -4,7 +4,7 @@ var app = express();
 var bodyParser = require('body-parser');
 var urlencodedParser=bodyParser.urlencoded({extended : false});
 
-var port    =   process.env.PORT || 22;
+var port    =   process.env.PORT || 8082;
 
 // MySQL connection
 
